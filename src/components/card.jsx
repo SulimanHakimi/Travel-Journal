59e79212b789827ccbb2360fb117476602fa82a1
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-04-15 19:23:54 +0430

    fix gaps

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,7 +5,7 @@ function Card(data) {
   return (
     <div className="bg-white lg:border-b-2 px-2 lg:border-gray-300 lg:flex-row flex-col  lg:w-3/5 w-full h-fit lg:h-64 flex items-center">
       <img className="lg:h-5/6 lg:w-1/4 h-2/6 w-5/6 rounded-lg flex" src={data.image} alt="" />
-      <div className="flex flex-col h-full lg:px-4 py-5 w-5/6 justify-around">
+      <div className="flex flex-col h-full lg:px-4 gap-4 py-5 w-5/6 justify-around">
         <div className="flex flex-col gap-1">
           <div className="flex gap-4 items-center">
             <span className="flex items-center text-base gap-1">
@@ -19,7 +19,7 @@ function Card(data) {
           <h1 className="lg:text-3xl text-2xl font-bold">{data.location}</h1>
         </div>
         <div className="flex flex-col gap-1">
-          <span className="font-semibold">{data.date}</span>
+          <span className="font-semibold text-[#423f3f]">{data.date}</span>
           <p className="text-base">{data.desc}</p>
         </div>
       </div>
