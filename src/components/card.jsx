97c395fd2d8394a015d5891b1a08cf4694283b54
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-04-15 20:01:07 +0430

    update text and info...

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,7 +5,7 @@ function Card(data) {
   return (
     <div className="bg-white lg:border-b-2 px-2 lg:border-gray-300 lg:flex-row flex-col  lg:w-3/5 w-full h-fit lg:h-64 flex items-center">
       <img className="lg:h-5/6 lg:w-1/4 h-2/6 w-5/6 rounded-lg flex" src={data.image} alt="" />
-      <div className="flex flex-col h-full lg:px-4 gap-4 py-5 w-5/6 justify-around">
+      <div className="flex flex-col h-full lg:px-4 lg:gap-0 gap-4 py-5 w-5/6 justify-around">
         <div className="flex flex-col gap-1">
           <div className="flex gap-4 items-center">
             <span className="flex items-center text-base gap-1">
